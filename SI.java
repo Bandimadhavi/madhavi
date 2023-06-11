b279f55f65ce7abda48f138a5ca81c5cf13250db
Bandimadhavi <131268251+Bandimadhavi@users.noreply.github.com>
2023-06-11 09:13:56 +0530

    Add files via upload

--- SI.java
+++ SI.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
-class Temperature{
+class SI{
 public static void main(String args[]){
 Scanner sc=new Scanner(System.in);
-int c=sc.nextInt();
-double f=9.0*c/5.0+32;
-System.out.println(f);
-}
-}
+int p=sc.nextDouble();
+double r=sc.nextDouble();
+double t=sc.nextDouble();
