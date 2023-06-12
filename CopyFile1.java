476389d76cd0338dc43629c8b6aa51cde0347735
Bandimadhavi <131268251+Bandimadhavi@users.noreply.github.com>
2023-06-12 17:15:50 +0530

    Add files via upload

---
+++ CopyFile1.java
@@ -0,0 +1,15 @@
+class CopyFile1{
+public static void main(String args[])throws Exception{
+FileReader fr=new FileReader("D:\\fh\\Demo.txt");
+BufferedReader br=new BufferedReader(fr);
+CharArrayWriter out=new CharArrayWriter();
+int i;
+while((i=br.read())!=-1){
+out.write((char)i);
+}
+FileWriter f1=new FileWriter("D:\\fh\\z.txt");
+out.writeTo(f1);
+f1.close();
+System.out.println("Success...");
+}
+}
